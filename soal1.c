4c23e032e1da3ea33f8b3e4b4e717b66e6604e46
thur055 <corneliusarthur226@gmail.com>
2026-04-20 09:13:16 +0700

    Update soal1.c
    1st try

--- soal1.c
+++ soal1.c
@@ -1,5 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    return 0;
+#define MAX_DATA 512
+int search_len(int *arrin, int N){
+    int arrout[N];
+    int j = 0;
+    for (int i = 0; i<N; i++){
+        if(arrin[i]==-1){
+            arrout[j] = i;
+            j++;
+        }
+    }
+    return j;
 }
+
+int main(){
+    int N;
+    int j = 0;
+    scanf("%d ",&N);
+    int arr[N];
+    int arrout[N]; 
+    for(int i = 0; i< N-1; i++){
+        scanf("%d ", &arr[i]);
+    }
+    scanf("%d", &arr[N-1]);
+    for(int i = 0; i< N; i++){
+        arrout[i] = arr[i];
+    }
+    j = search_len(arr, N);
+    int arridx[j];
+    int k = 0;
+    for(int i = 0; i<N-1;i++){
+        if (arr[i]==-1){
+            arridx[k] = i;
+            k++;
+        }
+    
+    }
+    /*for(int i = 0; i<j; i++){
+        printf("%d ", arridx[i]);
+    }
+        */
+    for (int i = 0; i<j;i++){
+        if (arridx[0] == 0){
+            if(arr[1]== -1){
+                arrout[0] = 0;
+            }
+            else{
+                arrout[0] = arrout[1];
+            }
+        }
+        else{
+            if(arr[arridx[i]+1]==-1 && arr[arridx[i]-1]==-1){
+                arrout[arridx[i]] = 0;
+                k++;
+            }
+            else if (arr[arridx[i]+1]==-1 && !(arr[arridx[i]-1]==-1))
+            {
+                arrout[arridx[i]] = arr[arridx[i]-1];
+                k++;
+            }
+            else if (!(arr[arridx[i]+1]==-1) && arr[arridx[i]-1]==-1)
+            {
+                arrout[arridx[i]] = arr[arridx[i]+1];
+                k++;
+            }
+            else if (!(arr[arridx[i]+1]==-1) && !(arr[arridx[i]-1]==-1))
+            {
+                arrout[arridx[i]] = (arr[arridx[i]-1]+arr[arridx[i]+1])/2;
+                k++;
+            }
+        }
+    }
+    printf("RECOVERED ");
+    int sum = 0;
+    for(int i = 0; i < N;i++){
+        printf("%d ", arrout[i]);
+        sum = sum + arrout[i];
+    }
+    printf("\n");
+    printf("MAX_SUM %d", sum);
+
+}
+
